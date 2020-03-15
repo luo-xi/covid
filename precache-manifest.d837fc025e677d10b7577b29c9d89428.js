1295527db092dca15e77b6166515fcead1cde946
luo-xi <luoxi.nus.ece@gmail.com>
2020-03-15 18:04:55 +0800

    Updates

---
+++ precache-manifest.d837fc025e677d10b7577b29c9d89428.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dbe624adf5f3156188666bb8c01d93f",
+    "url": "/covid/index.html"
+  },
+  {
+    "revision": "ad5ef13287439ffa6847",
+    "url": "/covid/static/css/2.077befd8.chunk.css"
+  },
+  {
+    "revision": "c27c501244122001ce53",
+    "url": "/covid/static/css/main.2500f7e4.chunk.css"
+  },
+  {
+    "revision": "ad5ef13287439ffa6847",
+    "url": "/covid/static/js/2.d0063842.chunk.js"
+  },
+  {
+    "revision": "64733da8c79cb0ccec9eb6b0932aa58f",
+    "url": "/covid/static/js/2.d0063842.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c27c501244122001ce53",
+    "url": "/covid/static/js/main.14f9d709.chunk.js"
+  },
+  {
+    "revision": "cf639c81ddf81a688abc",
+    "url": "/covid/static/js/runtime-main.77de2087.js"
+  }
+]);
